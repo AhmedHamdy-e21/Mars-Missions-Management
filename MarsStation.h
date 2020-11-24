f8efe94deaf4594070495d9796ba45816db41322
Ahmed Hamdy <ahmedhamdy.e21@gmail.com>
2020-11-24 21:29:24 +0200

    Intialize Mars Station class

---
+++ MarsStation.h
@@ -0,0 +1,14 @@
+//
+// Created by raspberry on 2020-11-24.
+//
+
+#ifndef MARS_MISSIONS_MANAGEMENT_MARSSTATION_H
+#define MARS_MISSIONS_MANAGEMENT_MARSSTATION_H
+
+
+class MarsStation {
+
+};
+
+
+#endif //MARS_MISSIONS_MANAGEMENT_MARSSTATION_H
