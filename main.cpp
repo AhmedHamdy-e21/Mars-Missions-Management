d93c1b5bcafb2a4aab9fdb41dd015ee30a0f773f
Ahmed Hamdy <ahmedhamdy.e21@gmail.com>
2020-12-01 15:30:17 +0200

    Phase 1

--- main.cpp
+++ main.cpp
@@ -103,7 +103,51 @@ void loadFile(RoverList& RL,MissionList& ML,LinkedQueue<Mission>& CurrentE,Linke
 
 }
 
-void printInteractiveMode(int Day)
+
+
+#define MAX_size 100
+template<typename T>
+void Interactive(LinkedQueue <T> rovers, LinkedQueue<T> missions) {
+    T Rovers, Missions;
+    int i = 0;
+    int j = 0;
+    int x = 0;
+    int y = 0;
+    bool Qbool1, Q2bool;
+    int waitinglistmissions[MAX_size];
+    int waitinglistrovers[MAX_size];
+    int Executionlistmissions[MAX_size];
+    int Executionlistrovers[MAX_size];
+    Qbool1 = rovers.dequeue(Rovers);
+    Q2bool = missions.dequeue(Missions);
+    while (Qbool1 || Q2bool) {
+
+        if (!Qbool1 && Q2bool) {
+            waitinglistmissions[j] = Missions;
+            j = j + 1;
+
+            Q2bool = missions.dequeue(Missions);
+        } else if (Qbool1 && !Q2bool) {
+            waitinglistrovers[y] = Rovers;
+            y = y + 1;
+            Qbool1 = rovers.dequeue(Rovers);
+
+        } else if (Qbool1 && Q2bool) {
+            Executionlistmissions[i] = Missions;
+            Executionlistrovers[x] = Rovers;
+            i = i + 1;
+            x = x + 1;
+            Qbool1 = rovers.dequeue(Rovers);
+            Q2bool = missions.dequeue(Missions);
+        } else {
+            break;
+        }
+    }
+}
+
+
+
+    void printInteractiveMode(int Day)
 {
     cout<<"Current Day:   "<<Day<<endl;
 }
@@ -142,6 +186,7 @@ int main() {
         if(Mode==1)
         {
             printInteractiveMode(Day);
+            Interactive(LinkedQueue <RoverList> RL,  CurrentE)
         }
         Day++;
     }
